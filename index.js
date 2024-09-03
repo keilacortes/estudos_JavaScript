e78267654a2dd2a8800ac83e08b9c4b63a90aa6d
Keila Côrtes <keilawonka4@gmail.com>
2024-09-03 17:23:42 -0300

    initial

---
+++ index.js
@@ -0,0 +1,21 @@
+// exercício 1 - cadastro de recrutas
+
+let nome = prompt("Digite seu nome: ")
+let sobrenome = prompt("Digite seu sobrenome: ")
+let anoDeNascimento = prompt("Digite seu ano de nascimento: ")
+let campoDeEstudo = prompt("Digite seu campo de estudo: ")
+
+console.log("Recruta:",nome, sobrenome,anoDeNascimento)
+console.log("O campo de estudo do recruta é:",campoDeEstudo)
+
+
+// exercício 2 - calculadora de 4 operações
+
+let primeiroValor = prompt("Digite o primeiro número: ")
+let segundoValor = prompt("Digite o segundo número: ")
+const soma = primeiroValor + segundoValor
+const subtracao = primeiroValor - segundoValor
+const multiplicacao = primeiroValor * segundoValor
+const divisao = primeiroValor / segundoValor
+
+console.log("O resultado da soma é:",soma,"O resultado da subtração é", subtracao,"O resultado da multiplicação é:", multiplicacao,"O resultado da divisão é:",divisao)
