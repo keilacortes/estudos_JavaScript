41fb1c925f2ccd8541104f238d3264184a656813
Keila Côrtes <keilawonka4@gmail.com>
2024-10-01 00:24:45 -0300

    add praticando dom

--- exercicios-intermediarios/sistema de vagas/vagas.js
+++ exercicios-intermediarios/sistema de vagas/vagas.js
@@ -23,9 +23,9 @@ function criarVaga() {
     
     if (confirm("Nome da vaga: " + vaga.nome + "\nDescrição da vaga: " + vaga.descricao + "\nData limite: " + vaga.dataLimite + "\n\nAdicionar vaga?")) {
         vagas.push(vaga)
-        alert("Vaga adicionada")
+        alert("Vaga adicionada!")
     }else{
-        alert("Vaga não adicionada")
+        alert("Vaga não adicionada.")
     }
 }
 function visualizarVaga() {
@@ -92,6 +92,7 @@ function excluirVaga() {
         alert("Não há vagas!")
     }
 }
+
 do{
     opcoes = prompt("Vagas disponíveis: " + vagas.length + "\n\nEscolha uma opção:\n 1. Listar vagas disponíveis\n 2. Criar uma nova vaga\n 3. Visualizar uma vaga\n 4. Inscrever um candidato em uma vaga\n 5. Excluir uma vaga\n 6. Sair")
     switch (opcoes) {
